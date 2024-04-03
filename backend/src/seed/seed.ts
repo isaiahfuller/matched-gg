d4455c3ef873196052546e44aa0adfd1504089d9
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-04-03 14:31:11 -0500

    Update TwitchHandler import, add seed main function, and add drizzle init

--- backend/src/seed/seed.ts
+++ backend/src/seed/seed.ts
@@ -1,8 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
-import TwitchHandler from '../domain/twitch/handlers/twitchHandler';
+import TwitchHandler from '../infrastructure/twitch/handlers/twitchHandler';
 import { config } from '@config/config';
 import { logger } from 'src/util/logger';
+import { IgdbFacade } from 'src/infrastructure/igdb/facade/igdbFacade';m/pg-core';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+
+const client = new Client({
+  user: config.db.user,
+  host: config.db.host,
+  database: config.db.database,
+  password: config.db.password,
+  port: config.db.port,
+});
+
+const initDrizzle = async () => {
+  await client.connect();
+  const db = drizzle(client);
+  return db;
+};
 
 const connectToTwitch = async () => {
   const twitchHandler = new TwitchHandler(
@@ -13,9 +30,24 @@ const connectToTwitch = async () => {
     },
     logger,
   );
-  await twitchHandler.connect();
+  const response = await twitchHandler.connect();
+  return response;
 };
 
-connectToTwitch();
+const seed = async (): Promise<void> => {
+  const twitch = await connectToTwitch();
+  const igdb = new IgdbFacade(
+    { accessToken: twitch.access_token, clientId: config.twitch.clientId },
+    logger,
+    twitch.access_token,
+  );
+
+  const games = await igdb.seedGames({ concurrency: 4 });
+  console.log(games);
+
+  process.exit(0);
+};
+
+seed();
 
 // import db from './database/db';
