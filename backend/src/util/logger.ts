e99b0f29b041e26a9c41dc0fee8beb2628e20aa0
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-04-03 14:30:23 -0500

    Add pino-pretty transport option to logger

--- backend/src/util/logger.ts
+++ backend/src/util/logger.ts
@@ -5,6 +5,12 @@ const pinoOptions: LoggerOptions = {
   level: config.pinoOptions?.level || 'info',
   name: config.pinoOptions?.name || 'logger',
   enabled: config.pinoOptions?.enabled || true,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
 };
 
 export const logger = pino(pinoOptions);
