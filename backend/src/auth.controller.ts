2c7b4e5554c7e6ac1037100a619856a1673308cd
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-04-04 18:42:29 -0400

    add comments

--- backend/src/auth.controller.ts
+++ backend/src/auth.controller.ts
@@ -16,9 +16,12 @@ export class AuthController {
   private logger = new Logger(AuthController.name);
   constructor(private readonly authService: AuthService) {}
 
-  @Get('steam')
+  @Get('steam') // TODO: Change to Post when front-end is implemented
   @UseGuards(AuthGuard('steam'))
   @HttpCode(HttpStatus.OK)
+  /**
+   * @remarks this is never called due to the authguard sending user to the return route
+   */
   login() {}
 
   @Get('steam/return')
