79aa00ecb3f9dab39533d1bcac19b11a0f2a9f9d
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-04-25 16:37:54 -0400

    add mapArtworks

---
+++ backend/src/infrastructure/igdb/db/map/mapArtworks.ts
@@ -0,0 +1,15 @@
+import { Artworks } from '../schema/artworks';
+
+export const mapArtwork = (artwork) => {
+  const mappedArtwork = {
+    alphaChannel: artwork.alpha_channel,
+    animated: artwork.animated,
+    checksum: artwork.checksum,
+    game: artwork.game,
+    height: artwork.height,
+    imageId: artwork.image_id,
+    url: artwork.url,
+    width: artwork.width,
+  } satisfies Artworks;
+  return mappedArtwork;
+};
