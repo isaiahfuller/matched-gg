508869194909118414b2ac50f051606ff693f710
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-04-25 16:27:13 -0400

    add artwork fields

---
+++ backend/src/infrastructure/igdb/facade/subsystems/enums/fields/ArtworkFields.ts
@@ -0,0 +1,13 @@
+/**
+ * Enum representing the available fields for an artwork in the IGDB API.
+ */
+export enum ArtworkFields {
+  alpha_channel = 'alpha_channel',
+  animated = 'animated',
+  checksum = 'checksum',
+  game = 'game',
+  height = 'height',
+  image_id = 'image_id',
+  url = 'url',
+  width = 'width',
+}
