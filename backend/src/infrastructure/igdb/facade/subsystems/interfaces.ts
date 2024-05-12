1bcee1e0e13f0bb5b34b47e4b9e18bd21cf68ed5
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-11 21:18:03 -0500

    refactor count subsystem (#143)
    * feat: Add CountDTO interface IGDB count endpoints
    
    * feat: Add IgdbResources enum for IGDB API endpoints
    
    * feat: Refactor GetTotalCount to GetCount for IGDB API
    
    * chore: Refactor IGDBFacade to use GetCount for count endpoints
    
    * feat: Add IgdbGetCount interface for IGDB count endpoints
    
    * Remove unused GetAll interfaces
    
    * lint: remove unused WebsiteDTO

--- backend/src/infrastructure/igdb/facade/subsystems/interfaces.ts
+++ backend/src/infrastructure/igdb/facade/subsystems/interfaces.ts
@@ -2,8 +2,9 @@ import { Apicalypse, RequestAllConfig } from 'apicalypse';
 import { GameField } from './enums/fields/GameField';
 import { GameDTO } from './DTO/GameDTO';
 import { AxiosResponse } from 'axios';
-import { WebsiteDTO } from './DTO/WebsiteDTO';
 import { AllDTO } from './types';
+import { IgdbResources } from './enums/IgdbResources';
+import { CountDTO } from './DTO/CountDTO';
 
 export interface IGetGame {
   client: Apicalypse;
@@ -15,17 +16,9 @@ export interface IGetGame {
   ): Promise<GameDTO>;
 }
 
-export interface IGetTotalCount {
+export interface IgdbGetCount {
   client: Apicalypse;
-  execute(): Promise<number>;
-}
-
-export interface IGetTotalGameCount extends IGetTotalCount {
-  execute(): Promise<number>;
-}
-
-export interface IGetTotalWebsiteCount extends IGetTotalCount {
-  execute(): Promise<number>;
+  execute(resource: IgdbResources): Promise<CountDTO>;
 }
 
 export interface IGetManyGames {
@@ -48,22 +41,4 @@ export interface IGetAll {
     expanded?: boolean,
     totalGameCount?: number,
   ): Promise<AllDTO>;
-}
-
-export interface IGetAllGames extends IGetAll {
-  execute(
-    options: RequestAllConfig,
-    limit: number,
-    expanded?: boolean,
-    totalGameCount?: number,
-  ): Promise<GameDTO[]>;
-}
-
-export interface IGetAllWebsites extends IGetAll {
-  execute(
-    options: RequestAllConfig,
-    limit: number,
-    expanded?: boolean,
-    totalGameCount?: number,
-  ): Promise<WebsiteDTO[]>;
 }
