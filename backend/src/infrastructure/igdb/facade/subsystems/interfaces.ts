b8f08fbc6261332cdc36392531e5553161dbdb85
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-11 23:32:18 -0500

    feat: Remove unused import in igdb subsystem interfaces

--- backend/src/infrastructure/igdb/facade/subsystems/interfaces.ts
+++ backend/src/infrastructure/igdb/facade/subsystems/interfaces.ts
@@ -2,7 +2,6 @@ import { Apicalypse, RequestAllConfig } from 'apicalypse';
 import { GameField } from './enums/fields/GameField';
 import { GameDTO } from './DTO/GameDTO';
 import { AxiosResponse } from 'axios';
-import { AllDTO } from './types';
 import { IgdbResources } from './enums/IgdbResources';
 import { CountDTO } from './DTO/CountDTO';
 
