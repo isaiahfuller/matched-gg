45b59a4d170e1e431cc744e8e22d7042558428ba
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-14 15:35:46 -0500

    chore: Remove unused NRArtBaseDTO and update DTO imports (#157)

--- backend/src/infrastructure/igdb/facade/subsystems/DTO/CoversDTO.ts
+++ backend/src/infrastructure/igdb/facade/subsystems/DTO/CoversDTO.ts
@@ -1,7 +1,4 @@
-import { NRArtBaseDTO } from './ArtBaseDTO';
 import { ArtworkDTO } from './ArtworkDTO';
-
-export type NRCoversDTO = NRArtBaseDTO;
 
 export interface CoversDTO extends ArtworkDTO {
   game_localization: number;
