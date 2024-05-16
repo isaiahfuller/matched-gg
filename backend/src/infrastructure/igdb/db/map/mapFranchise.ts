027c94b01ba7df8202315274f257ede7e3a0da55
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-05-16 16:43:41 -0400

    remove createdAt

--- backend/src/infrastructure/igdb/db/map/mapFranchise.ts
+++ backend/src/infrastructure/igdb/db/map/mapFranchise.ts
@@ -4,7 +4,6 @@ export const mapFranchise = (franchise) => {
   const mappedFranchise = {
     checksum: franchise.checksum,
     games: franchise.games,
-    createdAt: new Date(),
     igdbCreatedAt: franchise.created_at
       ? new Date(franchise.created_at * 1000)
       : null,
