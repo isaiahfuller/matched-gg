fb432e8c1d545108ff2746da0a999c275f432f99
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-05-16 20:36:26 -0400

    change primary key

--- backend/src/infrastructure/igdb/db/schema/eventLogos.ts
+++ backend/src/infrastructure/igdb/db/schema/eventLogos.ts
@@ -5,7 +5,6 @@ import { eventsTable } from './events';
 export const eventLogoTable = pgTable('eventLogos', {
   ...commonArtFields,
   igdbCreatedAt: timestamp('igdb_created_at'),
-  igdbId: bigint('igdb_id', { mode: 'number' }).primaryKey(),
   igdbUpdatedAt: timestamp('igdb_updated_at'),
   event: bigint('event', { mode: 'number' }).references(
     () => eventsTable.igdbId,
