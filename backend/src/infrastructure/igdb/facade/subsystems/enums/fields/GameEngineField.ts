ed9a8b5af8712c2e557d6b07dbc1f03e8ff6dd1e
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-05-17 19:06:06 -0400

    updated fields

--- backend/src/infrastructure/igdb/facade/subsystems/enums/fields/GameEngineField.ts
+++ backend/src/infrastructure/igdb/facade/subsystems/enums/fields/GameEngineField.ts
@@ -10,3 +10,15 @@ export enum GameEngineField {
   updated_at = 'updated_at',
   url = 'url',
 }
+export enum ExpandedGameEngineField {
+  checksum = 'checksum',
+  companies = 'companies.*',
+  created_at = 'created_at',
+  description = 'description',
+  logo = 'logo',
+  name = 'name',
+  platforms = 'platforms.*',
+  slug = 'slug',
+  updated_at = 'updated_at',
+  url = 'url',
+}
