a76a299f07a3f57a96771caeffdbc96efb870db5
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-16 20:24:08 -0500

    refactor: sort keys options in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,10 +20,10 @@ async function bootstrap() {
 
   app.use(
     session({
-      store: redisStore,
-      secret: config.sessionSecret,
       resave: false,
       saveUninitialized: false,
+      secret: config.sessionSecret,
+      store: redisStore,
     }),
   );
   app.use(passport.initialize());
