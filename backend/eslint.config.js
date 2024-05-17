d65faf04d1b6916473701cae153960f11562298a
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-16 20:26:20 -0500

    feat: Enable key sorting in eslint

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,6 +2,8 @@ const tsplugin = require('@typescript-eslint/eslint-plugin');
 const tseslint = require('typescript-eslint');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 const eslintPluginPrettier = require('eslint-plugin-prettier');
+const tsSortKeys = require('eslint-plugin-typescript-sort-keys');
+const sortKeysPlus = require('eslint-plugin-sort-keys-plus');
 
 module.exports = tseslint.config({
   extends: [...tseslint.configs.recommended, eslintPluginPrettierRecommended],
@@ -19,10 +21,27 @@ module.exports = tseslint.config({
   },
   plugins: {
     '@typescript-eslint/eslint-plugin': tsplugin,
+    'sort-keys-plus': sortKeysPlus,
+    'typescript-sort-keys': tsSortKeys,
     prettier: eslintPluginPrettier,
   },
   ignores: ['**/eslint.config.js', 'dist/**/*'],
   rules: {
+    'typescript-sort-keys/interface': [
+      'warn',
+      'asc',
+      { caseSensitive: true, natural: false, requiredFirst: true },
+    ],
+    'sort-keys-plus/sort-keys': [
+      'warn',
+      'asc',
+      { caseSensitive: true, natural: false },
+    ],
+    'typescript-sort-keys/string-enum': [
+      'warn',
+      'asc',
+      { caseSensitive: true, natural: true },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
