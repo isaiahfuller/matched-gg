badedc4804d2d42289d9189cf28e280092ff65fd
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-16 20:19:33 -0500

    refactor: Sort map functions keys

--- backend/src/infrastructure/igdb/db/map/mapFranchise.ts
+++ backend/src/infrastructure/igdb/db/map/mapFranchise.ts
@@ -3,8 +3,8 @@ import { Franchises } from '../schema/franchise';
 export const mapFranchise = (franchise) => {
   const mappedFranchise = {
     checksum: franchise.checksum,
-    games: franchise.games,
     createdAt: new Date(),
+    games: franchise.games,
     igdbCreatedAt: franchise.created_at
       ? new Date(franchise.created_at * 1000)
       : null,
--- backend/src/infrastructure/igdb/db/map/mapWebsite.ts
+++ backend/src/infrastructure/igdb/db/map/mapWebsite.ts
@@ -2,16 +2,16 @@ import { WebsitePGEnum, Websites } from '../schema/websites';
 
 export const mapWebsite = (website) => {
   const mappedWebsite = {
-    igdbId: website.id,
     checksum: website.checksum,
     game: website.game,
+    igdbId: website.id,
+    trusted: website.trusted,
+    updatedAt: new Date(),
+    url: website.url,
     websiteCategory:
       website.category === undefined
         ? null
         : WebsitePGEnum.enumValues[website.category],
-    trusted: website.trusted,
-    updatedAt: new Date(),
-    url: website.url,
   } satisfies Websites;
   return mappedWebsite;
 };
