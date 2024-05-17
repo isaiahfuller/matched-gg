e90998dde797f075df15fe969407dc8c684518b1
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-16 19:37:42 -0500

    fix: Update logger configuration to remove pino-pretty transport option due to crashing on large logs

--- backend/src/util/logger.ts
+++ backend/src/util/logger.ts
@@ -5,12 +5,8 @@ const pinoOptions: LoggerOptions = {
   level: config.pinoOptions?.level || 'info',
   name: config.pinoOptions?.name || 'logger',
   enabled: config.pinoOptions?.enabled || true,
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
 };
 
 export const logger = pino(pinoOptions);
+
+export default logger;
