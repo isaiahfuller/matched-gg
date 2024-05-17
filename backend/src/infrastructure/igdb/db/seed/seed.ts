e607a88697fd51c8952602cc0263c524fba69600
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-17 09:55:37 -0500

    refactor: Reorganize imports and sort keys in seed.ts

--- backend/src/infrastructure/igdb/db/seed/seed.ts
+++ backend/src/infrastructure/igdb/db/seed/seed.ts
@@ -1,22 +1,23 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
-import TwitchHandler from '../../../twitch/handlers/twitchHandler';
 import { config } from '@config/config';
-import { logger } from 'src/util/logger';
-import { IgdbFacade } from 'src/infrastructure/igdb/facade/igdbFacade';
-import * as gamesSchema from '../schema/games';
-import * as websitesSchema from '../schema/websites';
-import * as artworksSchema from '../schema/artworks';
-import { mapGame } from '../map/mapGame';
+import { IgdbConfig } from '@config/interfaces';
 import { chunk } from '@util/chunk';
-import { IgdbDbController } from '../controller/IgdbDbController';
+import { IgdbFacade } from 'src/infrastructure/igdb/facade/igdbFacade';
+import { logger } from 'src/util/logger';
+
+import TwitchHandler from '../../../twitch/handlers/twitchHandler';
+import { ArtworkDTO } from '../../facade/subsystems/DTO/ArtworkDTO';
 import { GameDTO } from '../../facade/subsystems/DTO/GameDTO';
 import { WebsiteDTO } from '../../facade/subsystems/DTO/WebsiteDTO';
-import { mapWebsite } from '../map/mapWebsite';
-import { ArtworkDTO } from '../../facade/subsystems/DTO/ArtworkDTO';
-import { mapArtwork } from '../map/mapArtwork';
-import { IgdbConfig } from '@config/interfaces';
 import { IgdbResources } from '../../facade/subsystems/enums/IgdbResources';
+import { IgdbDbController } from '../controller/IgdbDbController';
+import { mapArtwork } from '../map/mapArtwork';
+import { mapGame } from '../map/mapGame';
+import { mapWebsite } from '../map/mapWebsite';
+import * as artworksSchema from '../schema/artworks';
+import * as gamesSchema from '../schema/games';
+import * as websitesSchema from '../schema/websites';
 
 const seed = async (): Promise<void> => {
   const igdbDbController = new IgdbDbController();
