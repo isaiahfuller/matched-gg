980c3fce39aa142f19952bfe2303f9186d52636f
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-05-17 10:31:49 -0500

    refactor: Update import order and sort keys in logger.ts

--- backend/src/util/logger.ts
+++ backend/src/util/logger.ts
@@ -1,5 +1,5 @@
-import { pino, LoggerOptions } from 'pino';
 import { config } from '@config/config';
+import { LoggerOptions, pino } from 'pino';
 
 const pinoOptions: LoggerOptions = {
   enabled: config.pinoOptions?.enabled || true,
