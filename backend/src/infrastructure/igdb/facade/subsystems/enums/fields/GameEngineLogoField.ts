7db9cb4eb6cc06642328750b80acd4b5004a8f56
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-06-30 14:22:00 -0400

    Add game engine logo table, fields

---
+++ backend/src/infrastructure/igdb/facade/subsystems/enums/fields/GameEngineLogoField.ts
@@ -0,0 +1,20 @@
+export enum GameEngineLogoField {
+  alpha_channel = 'alpha_channel',
+  animated = 'animated',
+  checksum = 'checksum',
+  height = 'height',
+  image_id = 'image_id',
+  url = 'url',
+  width = 'width',
+}
+
+export enum ExpandedGameEngineLogoField {
+  alpha_channel = 'alpha_channel',
+  animated = 'animated',
+  checksum = 'checksum',
+  height = 'height',
+  id = 'id',
+  image_id = 'image_id',
+  url = 'url',
+  width = 'width',
+}
