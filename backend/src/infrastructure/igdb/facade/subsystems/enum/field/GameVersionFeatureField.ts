15cb7b7090c3ba9f4f43013bd3dc31b0e3cf0426
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2024-07-01 19:31:54 -0400

    add game version feature

---
+++ backend/src/infrastructure/igdb/facade/subsystems/enum/field/GameVersionFeatureField.ts
@@ -0,0 +1,18 @@
+export enum GameVersionFeatureField {
+  category = 'category',
+  checksum = 'checksum',
+  description = 'description',
+  id = 'id',
+  position = 'position',
+  title = 'title',
+  values = 'values',
+}
+export enum ExpandedGameVersionFeatureField {
+  category = 'category',
+  checksum = 'checksum',
+  description = 'description',
+  id = 'id',
+  position = 'position',
+  title = 'title',
+  values = 'values.*',
+}
