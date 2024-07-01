2b0065beaddd8436167232356fe8dd8096421bcb
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2024-07-01 19:53:32 -0400

    add table

---
+++ backend/src/infrastructure/igdb/db/schema/gameVideos.ts
@@ -0,0 +1,13 @@
+import { bigint, pgTable, text } from 'drizzle-orm/pg-core';
+
+import { gamesTable } from './games';
+
+export const gameVideosTable = pgTable('gameVideos', {
+  checksum: text('checksum'),
+  game: bigint('game', { mode: 'number' }).references(() => gamesTable.igdbId),
+  igdbId: bigint('igdb_id', { mode: 'number' }).primaryKey(),
+  name: text('name').notNull(),
+  video_id: text('url'),
+});
+
+export type GameVideos = typeof gameVideosTable.$inferInsert;
