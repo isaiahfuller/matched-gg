9966b58b30b7b2876eef01aa682f7125362fa46c
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2024-07-01 15:03:12 -0400

    remove unchanged enum, add id

--- backend/src/infrastructure/igdb/facade/subsystems/enum/field/GameEngineLogoField.ts
+++ backend/src/infrastructure/igdb/facade/subsystems/enum/field/GameEngineLogoField.ts
@@ -3,16 +3,6 @@ export enum GameEngineLogoField {
   animated = 'animated',
   checksum = 'checksum',
   height = 'height',
-  image_id = 'image_id',
-  url = 'url',
-  width = 'width',
-}
-
-export enum ExpandedGameEngineLogoField {
-  alpha_channel = 'alpha_channel',
-  animated = 'animated',
-  checksum = 'checksum',
-  height = 'height',
   id = 'id',
   image_id = 'image_id',
   url = 'url',
