518185e3eaf7d9c3ca5673636dc80416fafbe9ca
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2024-07-03 14:26:17 -0400

    reorder fields

--- backend/src/infrastructure/igdb/facade/subsystems/enum/field/GenreField.ts
+++ backend/src/infrastructure/igdb/facade/subsystems/enum/field/GenreField.ts
@@ -1,9 +1,9 @@
 export enum GenreField {
   checksum = 'checksum',
   created_at = 'created_at',
+  id = 'id',
   name = 'name',
   slug = 'slug',
   updated_at = 'updated_at',
   url = 'url',
-  id = 'id',
 }
