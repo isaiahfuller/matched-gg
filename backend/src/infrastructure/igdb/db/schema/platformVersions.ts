02e43ea8a19888fd3aee4440a889c7de81fdefe6
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2024-07-08 11:39:57 -0400

    add table, map, field

---
+++ backend/src/infrastructure/igdb/db/schema/platformVersions.ts
@@ -0,0 +1,30 @@
+import { bigint, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const platformVersionsTable = pgTable('platformVersions', {
+  checksum: text('checksum'),
+  companies: bigint('companies', { mode: 'number' }).array(), // reference platform version company
+  connectivity: text('connectivity'),
+  cpu: text('cpu'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  graphics: text('graphics'),
+  igdbId: bigint('igdb_id', { mode: 'number' }).primaryKey(),
+  mainManufacturer: bigint('main_manufacturer', { mode: 'number' }), // reference platform version company
+  media: text('media'),
+  memory: text('memory'),
+  name: text('name').notNull(),
+  os: text('os'),
+  output: text('output'),
+  platformLogo: bigint('platform_logo', { mode: 'number' }), // reference platform logo
+  platformVersionReleaseDates: bigint('platform_version_release_dates', {
+    mode: 'number',
+  }).array(), // reference platform version company
+  resolutions: text('resolutions'),
+  slug: text('slug'),
+  sound: text('sound'),
+  storage: text('storage'),
+  summary: text('summary'),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  url: text('url'),
+});
+
+export type PlatformVersions = typeof platformVersionsTable.$inferInsert;
