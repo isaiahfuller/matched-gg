32b1563e0df9d0e6a31b90b4ad305a2d0ca2df4e
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2024-07-08 10:38:46 -0400

    add table, map, field

---
+++ backend/src/infrastructure/igdb/db/schema/networkTypes.ts
@@ -0,0 +1,18 @@
+import { bigint, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+
+import { eventNetworksTable } from './eventNetwork';
+
+export const networkTypesTable = pgTable('networkTypes', {
+  checksum: text('checksum'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  eventNetworks: bigint('event_networks', { mode: 'number' })
+    .notNull()
+    .references(() => eventNetworksTable.igdbId),
+  igdbCreatedAt: timestamp('igdb_created_at'),
+  igdbId: bigint('igdb_id', { mode: 'number' }).primaryKey(),
+  igdbUpdatedAt: timestamp('igdb_updated_at'),
+  name: text('name').notNull(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export type NetworkTypes = typeof networkTypesTable.$inferInsert;
