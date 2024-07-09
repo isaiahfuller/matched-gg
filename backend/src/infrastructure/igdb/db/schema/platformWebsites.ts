53bc68531a62c3faa48118542462cf663cc76ae4
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2024-07-09 12:26:07 -0400

    add table, map, field

---
+++ backend/src/infrastructure/igdb/db/schema/platformWebsites.ts
@@ -0,0 +1,45 @@
+import {
+  bigint,
+  boolean,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+
+export const PlatformWebsiteCategoryPGEnum = pgEnum(
+  'PlatformWebsiteCategoryEnum',
+  [
+    'official',
+    'wikia',
+    'wikipedia',
+    'facebook',
+    'twitter',
+    'twitch',
+    'instagram',
+    'youtube',
+    'iphone',
+    'ipad',
+    'android',
+    'steam',
+    'reddit',
+    'discord',
+    'google_plus',
+    'tumblr',
+    'linkedin',
+    'pinterest',
+    'soundcloud',
+  ],
+);
+
+export const platformWebsitesTable = pgTable('platformWebsites', {
+  category: PlatformWebsiteCategoryPGEnum('category'),
+  checksum: text('checksum'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  igdbId: bigint('igdb_id', { mode: 'number' }).primaryKey(),
+  trusted: boolean('trusted'),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  url: text('url'),
+});
+
+export type PlatformWebsites = typeof platformWebsitesTable.$inferInsert;
---
+++ backend/src/infrastructure/igdb/facade/subsystems/enum/field/PlatformWebsiteField.ts
@@ -0,0 +1,7 @@
+export enum PlatformWebsiteField {
+  category = 'category',
+  checksum = 'checksum',
+  id = 'id',
+  trusted = 'trusted',
+  url = 'url',
+}
