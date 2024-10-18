7729483a516ce20e3d911a68a691f0e8fc23ff58
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2024-10-18 15:41:27 -0400

    add authservice to test providers

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { LocalService } from './auth/strategies/local/local.service';
 import { UsersService } from './users/users.service';
 
@@ -12,7 +13,13 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService, LocalService, UsersService, JwtService],
+      providers: [
+        AppService,
+        LocalService,
+        UsersService,
+        JwtService,
+        AuthService,
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
