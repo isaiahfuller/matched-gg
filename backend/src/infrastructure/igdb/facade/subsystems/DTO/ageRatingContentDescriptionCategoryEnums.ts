359ce5ba696586e4dbf7d71c9fe063dfe0fa58b9
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2025-05-10 18:47:17 +0000

    fix error in enum

--- backend/src/infrastructure/igdb/facade/subsystems/DTO/ageRatingContentDescriptionCategoryEnums.ts
+++ backend/src/infrastructure/igdb/facade/subsystems/DTO/ageRatingContentDescriptionCategoryEnums.ts
@@ -1,6 +1,6 @@
 /**
  * IGDB API
- * This OpenAPI documentation and schema project IS IN BETA and maintained by [s-crypt.](https://github.com/s-crypt)  The source code and more information is [available in the GitHub repository](https://github.com/s-crypt/IGDB-OpenAPI)  Issues and pull requests are welcome! Please report any bugs or discrepancies you come across! 
+ * This OpenAPI documentation and schema project IS IN BETA and maintained by [s-crypt.](https://github.com/s-crypt)  The source code and more information is [available in the GitHub repository](https://github.com/s-crypt/IGDB-OpenAPI)  Issues and pull requests are welcome! Please report any bugs or discrepancies you come across!
  *
  * The version of the OpenAPI document: 4.0.0
  * Contact: support@igdb.com
@@ -10,97 +10,183 @@
  * Do not edit the class manually.
  */
 
-
 /**
- * The age rating content description enums   1: ESRB_alcohol_reference   2: ESRB_animated_blood   3: ESRB_blood   4: ESRB_blood_and gore   5: ESRB_cartoon_violence   6: ESRB_comic_mischief   7: ESRB_crude_humor   8: ESRB_drug_reference   9: ESRB_fantasy_violence   10: ESRB_intense_violence   11: ESRB_language   12: ESRB_lyrics   13: ESRB_mature_humor   14: ESRB_nudity   15: ESRB_partial_nudity   16: ESRB_real_gambling   17: ESRB_sexual_content   18: ESRB_sexual_themes   19: ESRB_sexual_violence   20: ESRB_simulated_gambling   21: ESRB_strong_language   22: ESRB_strong_lyrics   23: ESRB_strong_sexual content   24: ESRB_suggestive_themes   25: ESRB_tobacco_reference   26: ESRB_use_of alcohol   27: ESRB_use_of drugs   28: ESRB_use_of tobacco   29: ESRB_violence   30: ESRB_violent_references   31: ESRB_animated_violence   32: ESRB_mild_language   33: ESRB_mild_violence   34: ESRB_use_of drugs and alcohol   35: ESRB_drug_and alcohol reference   36: ESRB_mild_suggestive themes   37: ESRB_mild_cartoon violence   38: ESRB_mild_blood   39: ESRB_realistic_blood and gore   40: ESRB_realistic_violence   41: ESRB_alcohol_and tobacco reference   42: ESRB_mature_sexual themes   43: ESRB_mild_animated violence   44: ESRB_mild_sexual themes   45: ESRB_use_of alcohol and tobacco   46: ESRB_animated_blood and gore   47: ESRB_mild_fantasy violence   48: ESRB_mild_lyrics   49: ESRB_realistic_blood   50: PEGI_violence   51: PEGI_sex   52: PEGI_drugs   53: PEGI_fear   54: PEGI_discrimination   55: PEGI_bad_language   56: PEGI_gambling   57: PEGI_online_gameplay   58: PEGI_in_game_purchases   59: CERO_love   60: CERO_sexual_content   61: CERO_violence   62: CERO_horror   63: CERO_drinking_smoking   64: CERO_gambling   65: CERO_crime   66: CERO_controlled_substances   67: CERO_languages_and others   68: GRAC_sexuality   69: GRAC_violence   70: GRAC_fear_horror_threatening   71: GRAC_language   72: GRAC_alcohol_tobacco_drug   73: GRAC_crime_anti_social   74: GRAC_gambling   75: CLASS_IND_violencia   76: CLASS_IND_violencia_extrema   77: CLASS_IND_conteudo_sexual   78: CLASS_IND_nudez   79: CLASS_IND_sexo   80: CLASS_IND_sexo_explicito   81: CLASS_IND_drogas   82: CLASS_IND_drogas_licitas   83: CLASS_IND_drogas_ilicitas   84: CLASS_IND_linguagem_impropria   85: CLASS_IND_atos_criminosos   
+ * The age rating content description enums   1: ESRB_alcohol_reference   2: ESRB_animated_blood   3: ESRB_blood   4: ESRB_blood_and gore   5: ESRB_cartoon_violence   6: ESRB_comic_mischief   7: ESRB_crude_humor   8: ESRB_drug_reference   9: ESRB_fantasy_violence   10: ESRB_intense_violence   11: ESRB_language   12: ESRB_lyrics   13: ESRB_mature_humor   14: ESRB_nudity   15: ESRB_partial_nudity   16: ESRB_real_gambling   17: ESRB_sexual_content   18: ESRB_sexual_themes   19: ESRB_sexual_violence   20: ESRB_simulated_gambling   21: ESRB_strong_language   22: ESRB_strong_lyrics   23: ESRB_strong_sexual content   24: ESRB_suggestive_themes   25: ESRB_tobacco_reference   26: ESRB_use_of alcohol   27: ESRB_use_of drugs   28: ESRB_use_of tobacco   29: ESRB_violence   30: ESRB_violent_references   31: ESRB_animated_violence   32: ESRB_mild_language   33: ESRB_mild_violence   34: ESRB_use_of drugs and alcohol   35: ESRB_drug_and alcohol reference   36: ESRB_mild_suggestive themes   37: ESRB_mild_cartoon violence   38: ESRB_mild_blood   39: ESRB_realistic_blood and gore   40: ESRB_realistic_violence   41: ESRB_alcohol_and tobacco reference   42: ESRB_mature_sexual themes   43: ESRB_mild_animated violence   44: ESRB_mild_sexual themes   45: ESRB_use_of alcohol and tobacco   46: ESRB_animated_blood and gore   47: ESRB_mild_fantasy violence   48: ESRB_mild_lyrics   49: ESRB_realistic_blood   50: PEGI_violence   51: PEGI_sex   52: PEGI_drugs   53: PEGI_fear   54: PEGI_discrimination   55: PEGI_bad_language   56: PEGI_gambling   57: PEGI_online_gameplay   58: PEGI_in_game_purchases   59: CERO_love   60: CERO_sexual_content   61: CERO_violence   62: CERO_horror   63: CERO_drinking_smoking   64: CERO_gambling   65: CERO_crime   66: CERO_controlled_substances   67: CERO_languages_and others   68: GRAC_sexuality   69: GRAC_violence   70: GRAC_fear_horror_threatening   71: GRAC_language   72: GRAC_alcohol_tobacco_drug   73: GRAC_crime_anti_social   74: GRAC_gambling   75: CLASS_IND_violencia   76: CLASS_IND_violencia_extrema   77: CLASS_IND_conteudo_sexual   78: CLASS_IND_nudez   79: CLASS_IND_sexo   80: CLASS_IND_sexo_explicito   81: CLASS_IND_drogas   82: CLASS_IND_drogas_licitas   83: CLASS_IND_drogas_ilicitas   84: CLASS_IND_linguagem_impropria   85: CLASS_IND_atos_criminosos
  */
-export type AgeRatingContentDescriptionCategoryEnums = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 | 81 | 82 | 83 | 84 | 85;
+export type AgeRatingContentDescriptionCategoryEnums =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16
+  | 17
+  | 18
+  | 19
+  | 20
+  | 21
+  | 22
+  | 23
+  | 24
+  | 25
+  | 26
+  | 27
+  | 28
+  | 29
+  | 30
+  | 31
+  | 32
+  | 33
+  | 34
+  | 35
+  | 36
+  | 37
+  | 38
+  | 39
+  | 40
+  | 41
+  | 42
+  | 43
+  | 44
+  | 45
+  | 46
+  | 47
+  | 48
+  | 49
+  | 50
+  | 51
+  | 52
+  | 53
+  | 54
+  | 55
+  | 56
+  | 57
+  | 58
+  | 59
+  | 60
+  | 61
+  | 62
+  | 63
+  | 64
+  | 65
+  | 66
+  | 67
+  | 68
+  | 69
+  | 70
+  | 71
+  | 72
+  | 73
+  | 74
+  | 75
+  | 76
+  | 77
+  | 78
+  | 79
+  | 80
+  | 81
+  | 82
+  | 83
+  | 84
+  | 85;
 
 export const AgeRatingContentDescriptionCategoryEnums = {
-    ESRB_alcohol_reference: 1 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_animated_blood: 2 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_blood: 3 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_blood_and_gore: 4 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_cartoon_violence: 5 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_comic_mischief: 6 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_crude_humor: 7 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_drug_reference: 8 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_fantasy_violence: 9 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_intense_violence: 10 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_language: 11 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_lyrics: 12 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mature_humor: 13 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_nudity: 14 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_partial_nudity: 15 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_real_gambling: 16 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_sexual_content: 17 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_sexual_themes: 18 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_sexual_violence: 19 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_simulated_gambling: 20 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_strong_language: 21 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_strong_lyrics: 22 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_strong_sexual_content: 23 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_suggestive_themes: 24 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_tobacco_reference: 25 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_use_of_alcohol: 26 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_use_of_drugs: 27 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_use_of_tobacco: 28 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_violence: 29 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_violent_references: 30 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_animated_violence: 31 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_language: 32 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_violence: 33 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_use_of_drugs_and_alcohol: 34 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_drug_and_alcohol_reference: 35 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_suggestive_themes: 36 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_cartoon_violence: 37 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_blood: 38 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_realistic_blood_and_gore: 39 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_realistic_violence: 40 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_alcohol_and_tobacco_reference: 41 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mature_sexual_themes: 42 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_animated_violence: 43 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_sexual_themes: 44 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_use_of_alcohol_and_tobacco: 45 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_animated_blood_and_gore: 46 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_fantasy violence: 47 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_mild_lyrics: 48 as AgeRatingContentDescriptionCategoryEnums,
-    ESRB_realistic_blood: 49 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_violence: 50 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_sex: 51 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_drugs: 52 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_fear: 53 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_discrimination: 54 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_bad_language: 55 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_gambling: 56 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_online_gameplay: 57 as AgeRatingContentDescriptionCategoryEnums,
-    PEGI_in_game_purchases: 58 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_love: 59 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_sexual_content: 60 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_violence: 61 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_horror: 62 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_drinking_smoking: 63 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_gambling: 64 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_crime: 65 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_controlled_substances: 66 as AgeRatingContentDescriptionCategoryEnums,
-    CERO_languages_and others: 67 as AgeRatingContentDescriptionCategoryEnums,
-    GRAC_sexuality: 68 as AgeRatingContentDescriptionCategoryEnums,
-    GRAC_violence: 69 as AgeRatingContentDescriptionCategoryEnums,
-    GRAC_fear_horror_threatening: 70 as AgeRatingContentDescriptionCategoryEnums,
-    GRAC_language: 71 as AgeRatingContentDescriptionCategoryEnums,
-    GRAC_alcohol_tobacco_drug: 72 as AgeRatingContentDescriptionCategoryEnums,
-    GRAC_crime_anti_social: 73 as AgeRatingContentDescriptionCategoryEnums,
-    GRAC_gambling: 74 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_violencia: 75 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_violencia_extrema: 76 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_conteudo_sexual: 77 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_nudez: 78 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_sexo: 79 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_sexo_explicito: 80 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_drogas: 81 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_drogas_licitas: 82 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_drogas_ilicitas: 83 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_linguagem_impropria: 84 as AgeRatingContentDescriptionCategoryEnums,
-    CLASS_IND_atos_criminosos: 85 as AgeRatingContentDescriptionCategoryEnums
+  CERO_controlled_substances: 66 as AgeRatingContentDescriptionCategoryEnums,
+  CERO_crime: 65 as AgeRatingContentDescriptionCategoryEnums,
+  CERO_drinking_smoking: 63 as AgeRatingContentDescriptionCategoryEnums,
+  CERO_gambling: 64 as AgeRatingContentDescriptionCategoryEnums,
+  CERO_horror: 62 as AgeRatingContentDescriptionCategoryEnums,
+  'CERO_languages_and others': 67 as AgeRatingContentDescriptionCategoryEnums,
+  CERO_love: 59 as AgeRatingContentDescriptionCategoryEnums,
+  CERO_sexual_content: 60 as AgeRatingContentDescriptionCategoryEnums,
+  CERO_violence: 61 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_atos_criminosos: 85 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_conteudo_sexual: 77 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_drogas: 81 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_drogas_ilicitas: 83 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_drogas_licitas: 82 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_linguagem_impropria: 84 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_nudez: 78 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_sexo: 79 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_sexo_explicito: 80 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_violencia: 75 as AgeRatingContentDescriptionCategoryEnums,
+  CLASS_IND_violencia_extrema: 76 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_alcohol_and_tobacco_reference:
+    41 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_alcohol_reference: 1 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_animated_blood: 2 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_animated_blood_and_gore: 46 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_animated_violence: 31 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_blood: 3 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_blood_and_gore: 4 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_cartoon_violence: 5 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_comic_mischief: 6 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_crude_humor: 7 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_drug_and_alcohol_reference:
+    35 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_drug_reference: 8 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_fantasy_violence: 9 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_intense_violence: 10 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_language: 11 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_lyrics: 12 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mature_humor: 13 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mature_sexual_themes: 42 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_animated_violence: 43 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_blood: 38 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_cartoon_violence: 37 as AgeRatingContentDescriptionCategoryEnums,
+  'ESRB_mild_fantasy violence': 47 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_language: 32 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_lyrics: 48 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_sexual_themes: 44 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_suggestive_themes: 36 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_mild_violence: 33 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_nudity: 14 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_partial_nudity: 15 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_real_gambling: 16 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_realistic_blood: 49 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_realistic_blood_and_gore: 39 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_realistic_violence: 40 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_sexual_content: 17 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_sexual_themes: 18 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_sexual_violence: 19 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_simulated_gambling: 20 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_strong_language: 21 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_strong_lyrics: 22 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_strong_sexual_content: 23 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_suggestive_themes: 24 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_tobacco_reference: 25 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_use_of_alcohol: 26 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_use_of_alcohol_and_tobacco:
+    45 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_use_of_drugs: 27 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_use_of_drugs_and_alcohol: 34 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_use_of_tobacco: 28 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_violence: 29 as AgeRatingContentDescriptionCategoryEnums,
+  ESRB_violent_references: 30 as AgeRatingContentDescriptionCategoryEnums,
+  GRAC_alcohol_tobacco_drug: 72 as AgeRatingContentDescriptionCategoryEnums,
+  GRAC_crime_anti_social: 73 as AgeRatingContentDescriptionCategoryEnums,
+  GRAC_fear_horror_threatening: 70 as AgeRatingContentDescriptionCategoryEnums,
+  GRAC_gambling: 74 as AgeRatingContentDescriptionCategoryEnums,
+  GRAC_language: 71 as AgeRatingContentDescriptionCategoryEnums,
+  GRAC_sexuality: 68 as AgeRatingContentDescriptionCategoryEnums,
+  GRAC_violence: 69 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_bad_language: 55 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_discrimination: 54 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_drugs: 52 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_fear: 53 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_gambling: 56 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_in_game_purchases: 58 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_online_gameplay: 57 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_sex: 51 as AgeRatingContentDescriptionCategoryEnums,
+  PEGI_violence: 50 as AgeRatingContentDescriptionCategoryEnums,
 };
-
