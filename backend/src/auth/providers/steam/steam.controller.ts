39ea32c7bbc92698c6fe41ef7964e8223441f8c4
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2025-06-16 15:01:51 -0400

    update key names to match passport-steam-openid

--- backend/src/auth/providers/steam/steam.controller.ts
+++ backend/src/auth/providers/steam/steam.controller.ts
@@ -83,7 +83,7 @@ export class SteamController {
       throw new UnauthorizedException('No Steam account linked');
     }
     const games = await this.steamHandler.getOwnedGames(
-      session.user.steam.steamid,
+      session.user.steam.steamId,
     );
     return games.games;
   }
